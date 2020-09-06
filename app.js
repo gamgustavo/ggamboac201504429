4f748e1e73fb616b761c49c04acccbf4cbb13f1d
gamgustavo <31523927+gamgustavo@users.noreply.github.com>
2020-09-06 08:36:15 -0600

    devops
    devops

--- app.js
+++ app.js
@@ -8,18 +8,10 @@ var path = __dirname + '/src/';
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
 
 router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
-
-
-app.use(express.static(path));
-app.use("/", router);
 
 app.listen(5000, function () {
   console.log('Devops Demo')
